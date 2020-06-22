27301e9719b3dd6eb7fd5fc2da13d2b89edf942d
vidict44 <vidict44@gmail.com>
2020-06-22 07:22:56 -0500

    Finished fib

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,17 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
+let memo = {
+    1:0,
+    2:1,
+}
 
+function fib(n) {
+    if(memo[n]||memo[n]===0){
+        return memo[n];
+    }
+    else {
+        memo[n] = fib(n - 1) + fib(n - 2);
+        return memo[n];
+    }
 }
