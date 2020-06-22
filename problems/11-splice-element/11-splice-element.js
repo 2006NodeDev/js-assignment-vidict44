663ba062c899261e693e9100cf7374e27f32b4f8
vidict44 <vidict44@gmail.com>
2020-06-22 15:32:44 -0500

    11 Fin

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,13 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
+//This function splices an element and returns the element it deleted from the array.
 function spliceElement(someArr, index) {
-  
+ return(someArr.splice(index, 1));
+ 
 }
+
+let fish = ['angel', 'clown', 'mandarin', 'sturgeon']
+
+console.log(spliceElement(fish, 3));
+console.log(fish);
