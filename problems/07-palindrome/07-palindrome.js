fad53d7d4e9083d7c1488bde7cc23112b5d5a78f
vidict44 <vidict44@gmail.com>
2020-06-22 15:08:47 -0500

    07 Fin

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,20 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
+
+//This function returns a true if a string reads the same backward as forward and false if it does not. 
 function isPalindrome(someStr) {
+
+    let fullStringLength = someStr.length;
+    let midStringLength = Math.floor(fullStringLength/2);
+    someStr = someStr.toLowerCase();
+    for(let i = 0; i <midStringLength; i++){
+        if(someStr[i] !== someStr[fullStringLength-1-i]){
+            return false;
+        }
+
+    }
+    return true;
   
 }
+
