e4092effc5d3dd8f15a7217f514142b7fdab234c
vidict44 <vidict44@gmail.com>
2020-06-22 14:51:53 -0500

    06 Fin

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -1,7 +1,10 @@
 /* 6. Even Number
 Define function: isEven(someNum)
 Return true if even, false if odd.
+
+//This function returns a boolean of true if the number is even and false if the number is odd w/o modulus.
 Do not use % operator. */
 function isEven(someNum) {
+    return !(someNum & 1) ? true:false;
   
 }
