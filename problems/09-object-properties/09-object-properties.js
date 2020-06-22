0d8ec42cd01b19f189ad355901e5a095ef9896aa
vidict44 <vidict44@gmail.com>
2020-06-22 16:31:33 -0500

    09 Fin

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,17 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
+
+//This function defines object properties and prints the properties and their values.
 function objectProperties(someObj) {
 
+    for(human in someObj){
+        let name = someObj.name
+        let age = someObj.age
+        return console.log(`My name is ${name} and my age is ${age}.`);
+    }
+
 }
+
+let human = {name: "Luna Lovegood", age: 18};
+(objectProperties(human));
